4091640a48fa82869d52e1fee111ff18bd65aeb2
Mike Cancelosi <mike.cancelosi@gmail.com>
2021-09-07 22:52:29 -0400

    Dragging tests

--- Quipu.Web/ClientApp/src/components/ProjectTaskList.vue
+++ Quipu.Web/ClientApp/src/components/ProjectTaskList.vue
@@ -26,14 +26,43 @@
                    </q-list>
                </q-btn-dropdown>
 
-               <q-space/>
+               <q-space />
 
-               <q-btn label="All Tasks" disable no-caps icon="o_check_circle_outline" dense/> 
-               <q-btn label="Filter" disable no-caps icon="o_filter_list" dense/> 
-               <q-btn label="Sort" disable no-caps icon="o_import_export" dense/> 
-               <q-btn label="Customize" disable no-caps icon="o_dashboard_customize" dense/> 
-               <q-btn disable no-caps icon="o_more_horiz" dense/> 
+               <q-btn label="All Tasks" disable no-caps icon="o_check_circle_outline" dense />
+               <q-btn label="Filter" disable no-caps icon="o_filter_list" dense />
+               <q-btn label="Sort" disable no-caps icon="o_import_export" dense />
+               <q-btn label="Customize" disable no-caps icon="o_dashboard_customize" dense />
+               <q-btn disable no-caps icon="o_more_horiz" dense />
            </div>
+
+           <div class="col">
+               <div class="col-3">
+                   <h3>To Do</h3>
+                   <draggable class="list-group"
+                              :list="list1"
+                              group="people"
+                              @change="log"
+                              itemKey="name">
+                       <template #item="{ element, index }">
+                           <div class="list-group-item">{{ element.name }} {{ index }}</div>
+                       </template>
+                   </draggable>
+               </div>
+
+               <div class="col-3">
+                   <h3>Doing</h3>
+                   <draggable class="list-group"
+                              :list="list2"
+                              group="people"
+                              @change="log"
+                              itemKey="name">
+                       <template #item="{ element, index }">
+                           <div class="list-group-item">{{ element.name }} {{ index }}</div>
+                       </template>
+                   </draggable>
+               </div>
+           </div>
+
        </q-page-container>
    </q-layout>
         
@@ -43,19 +72,37 @@
     .row > .q-btn{
         margin-right:20px;
     }
+
+    .buttons {
+        margin-top: 35px;
+    }
+
 </style>
 
 <script>
     import axios from 'axios'
+    import draggable from "vuedraggable";
 
     export default {
         name: "ProjectTaskList",
+        components: { draggable },
         props: {
             team: {},
         },
         data() {
             return {
-                sections: ["To Do","Doing", "Done"],
+                sections: ["To Do", "Doing", "Done"],
+                list1: [
+                    { name: "John", id: 1 },
+                    { name: "Joao", id: 2 },
+                    { name: "Jean", id: 3 },
+                    { name: "Gerard", id: 4 }
+                ],
+                list2: [
+                    { name: "Juan", id: 5 },
+                    { name: "Edgard", id: 6 },
+                    { name: "Johnson", id: 7 }
+                ]
             }
         },
         methods: {
@@ -69,6 +116,21 @@
                         alert(error);
                     });
             },
+            add: function () {
+                this.list.push({ name: "Juan" });
+            },
+            replace: function () {
+                this.list = [{ name: "Edgard" }];
+            },
+            clone: function (el) {
+                return {
+                    name: el.name + " cloned"
+                };
+            },
+            log: function (evt) {
+                window.console.log(evt);
+            },
+
             openNav() {
                 this.$emit("open-nav");
             },
--- Quipu.Web/ClientApp/src/main.js
+++ Quipu.Web/ClientApp/src/main.js
@@ -3,10 +3,12 @@ import App from './App.vue'
 import router from './router'
 import { Quasar } from 'quasar'
 import quasarUserOptions from './quasar-user-options'
+import VueDraggable from "vuedraggable"
 
 const app = createApp(App).use(Quasar, quasarUserOptions)
 
 app.use(router)
+app.use(VueDraggable)
 
 app.mount('#app')
 
